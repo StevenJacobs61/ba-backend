b1669ef64ea4c2849a86e776406995db093a62ec
StevenJAcobs61 <stevenjacobs61@gmail.com>
2024-01-11 21:39:55 +0000

    web config updated

--- src/main/java/com/blockarch/blockarch/config/WebConfig.java
+++ src/main/java/com/blockarch/blockarch/config/WebConfig.java
@@ -8,12 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://blockarch.vercel.app") // Replace with your frontend URL
+                .allowedOrigins("*") // Use "*" for testing, replace with actual frontend URL in production
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
+
